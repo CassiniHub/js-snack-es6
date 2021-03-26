70be65b8dea3357ea2dd9379b3e505cba50e89df
Marco Orazio Re <marcooraziore@gmail.com>
2021-03-26 16:27:57 +0100

    Ex #2 completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// * EX #1
 function minor(array) {
    
    let lightBike = {
@@ -5,8 +6,6 @@ function minor(array) {
       'name': '',
       'weight': Number.MAX_SAFE_INTEGER
    }
-
-   
 
    for (let i = 0; i < array.length; i++) {
       const Elem = array[i];
@@ -52,9 +51,100 @@ function es1() {
    console.log(`La bici più leggera è la ${lightBike.name}, pesante ${lightBike.weight} kg.`);
 }
 
+// - - - - - - - - - - - - - - - - - - - - - - - -
+
+// * EX #2
+function getRnd(min, max) {
+   
+   let intMin = min;
+   let intMax = max - min + 1;
+
+   let rnd = Math.floor((Math.random() * intMax) + intMin)
+
+   return rnd;
+}
+
+function rndParam(arrayObj) {
+   
+   for (let i = 0; i < arrayObj.length; i++) {
+      const Obj = arrayObj[i];
+      
+      Obj.points = getRnd(1,60);
+      Obj.gotFouls = getRnd(1,20);
+   }
+   return arrayObj;
+}
+
+function noPoints(arrayObj, newArrayObj) {
+   
+   for(let i = 0; i < arrayObj.length; i++){
+      
+      let Obj = arrayObj[i];
+   
+      const { name, gotFouls } = Obj;
+   
+      Obj = { name, gotFouls };
+   
+      newArrayObj.push(Obj);
+   }
+
+   return newArrayObj;
+}
+
+function es2() {
+   
+   // Creare un array di oggetti di squadre di calcio. 
+   // Ogni squadra avrà diverse proprietà: 
+   // nome, punti fatti, falli subiti.
+   // Nome sarà l'unica proprietà da compilare, 
+   // le altre saranno tutte settate a 0.
+
+   let teams = [
+
+      {
+         'name'    : 'Team1',
+         'points'  : '0',
+         'gotFouls': '0'
+      },
+      {
+         'name'    : 'Team2',
+         'points'  : '0',
+         'gotFouls': '0'
+      },
+      {
+         'name'    : 'Team3',
+         'points'  : '0',
+         'gotFouls': '0'
+      },
+      {
+         'name'    : 'Team4',
+         'points'  : '0',
+         'gotFouls': '0'
+      }
+   ];
+
+   // Generare numeri random al posto degli 0 nelle proprietà:
+   // punti fatti e falli subiti   
+
+   teams = rndParam(teams);
+
+   // console.log(teams);
+
+   // Usando la destrutturazione creiamo un nuovo array 
+   // i cui elementi contengono solo nomi e falli subiti 
+   // e stampiamo tutto in console.
+
+   let newTeams = [];
+
+   noPoints(teams, newTeams);
+
+   console.log(newTeams);
+}
+
 function init() {
    
-   es1()
+   // es1()
+   es2();
 }
 
 $(document).ready(init);
